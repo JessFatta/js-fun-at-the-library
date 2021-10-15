893b4470d6b459c34bdf932d3b1d8446f1695dcb
Jessica Fatta <jessicafatta@gmail.com>
2021-10-15 10:42:04 -0400

    Complete all tests

--- src/book.js
+++ src/book.js
@@ -4,12 +4,48 @@ function createTitle(bookIdea) {
 createTitle();
 
 
+function buildMainCharacter(name, age, pronouns) {
+  var mainCharacter = {
+    name: name,
+    age: age,
+    pronouns: pronouns
+  }
+  return mainCharacter;
+};
+
+
+function saveReview(newReview, reviews) {
+    if (reviews.includes(newReview)){
+      return reviews;
+    } else {
+      reviews.push(newReview);
+    }
+    return reviews;
+}
+
+function calculatePageCount(bookTitle){
+  return bookTitle.length * 20
+};
+
+function writeBook(bookTitle, bookCharacter, genre) {
+  var book = {
+    title: bookTitle,
+    mainCharacter: bookCharacter,
+    pageCount: bookTitle.length * 20,
+    genre: genre,
+  }
+  return book;
+};
+
+function editBook(book) {
+  return book.pageCount *= .75
+};
 
 module.exports = {
   createTitle,
-  // buildMainCharacter,
-  // saveReview,
-  // calculatePageCount,
-  // writeBook,
-  // editBook
+  buildMainCharacter,
+  saveReview,
+  calculatePageCount,
+  writeBook,
+  editBook
 }
