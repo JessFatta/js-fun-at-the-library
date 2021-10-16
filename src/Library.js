3c969733ea08b92d374e62b34b7877224505c73f
Jessica Fatta <jessicafatta@gmail.com>
2021-10-16 11:42:54 -0400

    Add book

--- src/Library.js
+++ src/Library.js
@@ -1,7 +1,36 @@
+function createLibrary(name) {
+  var library = {
+    name: name,
+    shelves: {
+      fantasy: [],
+      fiction: [],
+      nonFiction: [],
+    }
+  }
+  return library;
+}
+createLibrary();
+
+
+function addBook(library, title) {
+  library.shelves[title.genre].push(title)
+}
+
+
+function checkoutBook(library, title, shelf) {
+  var checkout = `You have now checked out ${title} from the ${library.name}`;
+  var unavailable = `Sorry, there are currently no copies of ${title} available at the ${library.name}`;
+  for (var i = 0; i < library.shelves[shelf].length; i++) {
+    if (title === library.shelves[shelf][i].title)
+    library.shelves[shelf].splice(i, 1);
+  return checkout;
+  }
+  return unavailable;
+}
 
 
 module.exports = {
-  // createLibrary,
-  // addBook,
-  // checkoutBook
+ createLibrary,
+ addBook,
+  checkoutBook
 };
--- src/shelf.js
+++ src/shelf.js
@@ -1,7 +1,42 @@
 
+function shelfBook(bookName, shelf) {
+  if (shelf.length < 3) {
+  shelf.unshift(bookName)
+  }
+};
+
+function unshelfBook(bookName, shelf) {
+ for (var i = 0; i < shelf.length; i++) {
+   if (bookName === shelf[i].title)  {
+      shelf.splice(i, 1);
+    }
+    }
+};
+
+function listTitles(shelf) {
+  var titles = []
+  for (var i = 0; i < shelf.length; i++) {
+    titles.push(shelf[i].title);
+
+
+ }
+ var create = titles.join(", ")
+ return create;
+};
+
+
+function searchShelf(shelf, title) {
+  for (var i = 0; i < shelf.length; i++) {
+    if (shelf[i].title === title) {
+      return true;
+      }
+    }
+    return false;
+  };
+
 module.exports = {
-  // shelfBook,
-  // unshelfBook,
-  // listTitles,
-  // searchShelf
+   shelfBook,
+ unshelfBook,
+  listTitles,
+searchShelf
 };
